63527e293823405b7f8add492ba6cb727ac2930e
ingabiremariesandrine187 <ingabiremariesandrine187@gmail.com>
2025-10-11 23:09:06 +0200

    footer alignment

--- src/i18n/FooterLocale.ts
+++ src/i18n/FooterLocale.ts
@@ -1,7 +1,7 @@
 const FooterLocale:any = {
     fr:{
         desc:"La créativité est notre passion et notre métier. Nous sommes animés par elle. Notre mission est de donner vie à vos idées, de renforcer votre présence en ligne et de susciter l'engagement de votre public. Avec une équipe d'experts passionnés, nous offrons une gamme de services adaptés à vos besoins. Contactez-nous pour démarrer votre prochain projet.",
-        title:"Nos Compétences",
+        key:"Nos Compétences",
         comptence1:"Conception de logos",
         competence2:"Affiches et flyers personnalisés",
         competence3:"Cartes de visite uniques",
@@ -33,7 +33,7 @@ const FooterLocale:any = {
     },
     en:{
         desc:"Creativity is our passion and our profession. We are driven by it. Our mission is to bring your ideas to life, strengthen your online presence and engage your audience. With a team of passionate experts, we offer a range of services tailored to your needs. Contact us to start your next project.",
-        title:"Our competences",
+        key:"Our competences",
         competence1:"Logo design",
         competence2:"Custom posters and flyers",
         competence3:"Unique business cards",
@@ -84,7 +84,7 @@ const FooterLocale:any = {
         competence16:"Inama zijyanye n'umutekano w'amakuru kuri murandasi",
         competence17:"Gufasha kugura ibintu ku masoko yo kuri murandasi",
         competence18: "Cartes de visite/Business cards/ Visiting cards",
-        competence20:"Gukora amashusho y’amamaza", 
+        competence20:"Gukora amashusho y’amamaza",  
         competence21:"Waba wikorera,Rwiyemezamirimo cyangwa umushoramari,umu 'content creator/createur de contenu'cyangwa umu 'influencer' ,ikigo/societe/company cyangwa se umuntu usanzwe  turi hano kugira ngo tugufashe guha ubuzima ibitekerezo byanyu,ndetse no kubafasha guhora muri imbere mubyo mukora.",
         competence22:"Umwuga wanyu turawushyigikiye",
         contact1:"Twandikire",
@@ -96,7 +96,7 @@ const FooterLocale:any = {
         },
          nl: {
     desc: "Creativiteit is onze passie en ons vak. Het drijft ons. Onze missie is om jouw ideeën tot leven te brengen, je online aanwezigheid te versterken en je publiek te betrekken. Met een team van gepassioneerde experts bieden wij een reeks diensten die zijn afgestemd op jouw behoeften. Neem contact met ons op om je volgende project te starten.",
-    title: "Onze competenties",
+    key: "Onze competenties",
     competence1: "Logo ontwerp",
     competence2: "Aangepaste posters en flyers",
     competence3: "Unieke visitekaartjes",
